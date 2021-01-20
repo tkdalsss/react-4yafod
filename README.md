81cf46a091c7f5cc4524d242b98b2ec68d8cd40c
tkdalsss <lsm991229@naver.com>
2021-01-20 20:01:20 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4yafod
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4yafod)
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) =>{
+    const{
+      target: {value}
+    } = event;
+    let willUpdate = true;
+    if(typeof validator === "function"){
+      willUpdate = validator(value);
+    }
+    if(willUpdate){
+      setValue(value);
+    }
+  }
+  return {value, onChange};
+}
+
+const App = () => {
+  const maxLen = value => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div classNAme="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name}/>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
